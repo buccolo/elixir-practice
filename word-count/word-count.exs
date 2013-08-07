1ba76ad423788de1842b934ba41745ceca0788f5
Bruno Buccolo <bruno.buccolo@gmail.com>
2013-08-07 20:10:17 -0300

    Improves code style

--- word-count/word-count.exs
+++ word-count/word-count.exs
@@ -1,6 +1,6 @@
 defmodule Words do
-  def count phrase do
-    words = split(phrase)
+  def count(phrase) do
+    words = split phrase
 
     List.foldl words, HashDict.new, fn(word, word_count) ->
       HashDict.update word_count, word, 1, fn(count) ->
@@ -9,7 +9,7 @@ defmodule Words do
     end
   end
 
-  defp split phrase do
+  defp split(phrase) do
     Regex.scan %r/[a-zA-Z0-9]+/, String.downcase(phrase)
   end
 end
